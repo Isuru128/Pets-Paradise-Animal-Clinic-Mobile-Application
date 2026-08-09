11a2c277e697ccd39b1c0be5a2108595aad88406
Isuru128 <isurudula28@gmail.com>
2026-08-10 04:17:53 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
@@ -9,7 +8,6 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 app.get('/', (req, res) => {
     res.json({ msg: 'Backend is running' });
